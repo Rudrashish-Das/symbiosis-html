4fe6488b22633f7ed86da67f4077b116b6ecf886
Rudrashish Das <rudrashishdas0@gmail.com>
2023-10-30 00:24:48 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,7 @@
+var picture = 0;
+setInterval(function () {
+    picture += 1;
+    var path = "./assets/class"+picture+"2.jpg";
+    $("#banner").css("background-image","url("+path+")");
+    picture %= 4;
+}, 5000);
